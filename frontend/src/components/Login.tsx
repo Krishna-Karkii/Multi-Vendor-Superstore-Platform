598b91f4dcb1fb9df49bac245e79f92e644c9814
Krishna-Karkii <krishnakarki1996@gmail.com>
2026-08-18 19:02:18 +0545

    understand react state updates

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Login() {
+export default function Login({}) {
+
+    const [status, setStatus] = useState('idle');
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+        setStatus('submitting');
+        setTimeout( () => {setStatus('success');}, 2000);
+        setTimeout( () => {setStatus('idle');}, 4000);
+    }
   return (
     <div>
       <h1>Login</h1>
+      <div>
+        <form id='form' onSubmit={handleSubmit}>
+            <label id="name">Name: </label>
+            <input type="text" id='Name'/>
+            <br />
+            <label id="email">Email: </label>
+            <input type="email"/>
+            <br />
+            <button id="button">Click me</button>
+        </form>
+        {
+            status === 'idle' && <p>
+                Status: Ready
+            </p>
+        }
+        {
+            status === 'success' && <p>
+                Status: Signed in!
+            </p>
+        }
+        {
+            status === 'submitting' && <p>
+                Status: Signing in...
+            </p>
+        }
+      </div>
     </div>
   );
 }
