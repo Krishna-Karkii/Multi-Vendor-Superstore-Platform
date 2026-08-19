b77b4d29ec5529722e3c2040d32b37cd6dc57890
Krishna-Karkii <krishnakarki1996@gmail.com>
2026-08-19 21:02:17 +0545

    Understood how state and hooks work

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 
-export default function Login({}) {
+export default async function Login({}) {
 
     const [status, setStatus] = useState('idle');
+    const [email, setEmail] = useState('');
     
     async function handleSubmit(event) {
         event.preventDefault();
@@ -19,7 +20,12 @@ export default function Login({}) {
             <input type="text" id='Name'/>
             <br />
             <label id="email">Email: </label>
-            <input type="email"/>
+            <input 
+            type='email'
+            value={email}
+            onChange={(event) => {
+                setEmail(event.target.value);
+            }}/>
             <br />
             <button id="button">Click me</button>
         </form>
@@ -42,3 +48,52 @@ export default function Login({}) {
     </div>
   );
 }
+
+// export function Login({}) {
+
+//     const [status, setStatus] = useState('idle');
+//     const [email, setEmail] = useState('');
+    
+//     async function handleSubmit(event) {
+//         event.preventDefault();
+//         setStatus('submitting');
+//         setTimeout( () => {setStatus('success');}, 2000);
+//         setTimeout( () => {setStatus('idle');}, 4000);
+//     }
+//   return (
+//     <div>
+//       <h1>Login</h1>
+//       <div>
+//         <form id='form' onSubmit={handleSubmit}>
+//             <label id="name">Name: </label>
+//             <input type="text" id='Name'/>
+//             <br />
+//             <label id="email">Email: </label>
+//             <input 
+//             type='email'
+//             value={email}
+//             onChange={(event) => {
+//                 setEmail(event.target.value);
+//             }}/>
+//             <br />
+//             <button id="button">Click me</button>
+//         </form>
+//         {
+//             status === 'idle' && <p>
+//                 Status: Ready
+//             </p>
+//         }
+//         {
+//             status === 'success' && <p>
+//                 Status: Signed in!
+//             </p>
+//         }
+//         {
+//             status === 'submitting' && <p>
+//                 Status: Signing in...
+//             </p>
+//         }
+//       </div>
+//     </div>
+//   );
+// }
