5e516d0ab56ec2b1a1f0f20f9cce877d6a04e3c8
Krishna-Karkii <krishnakarki1996@gmail.com>
2026-09-17 23:42:35 +0545

    refactor and add logic to create user

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,10 +2,14 @@ import asyncpg
 import os
 from fastapi import Request
 from dotenv import load_dotenv
+import logging
+
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
 async def get_connection_pool():
+    """Create a database connection pool to manage connections."""
     return await asyncpg.create_pool(
             os.getenv("DB_URL"),
             min_size=5,
@@ -13,15 +17,28 @@ async def get_connection_pool():
         )
 
 
-async def get_user_by_email(
-        connnection: asyncpg.Connection,
+async def create_user(
+        conn: asyncpg.Connection,
         email: str,
-        password: str
-):
-    return await connnection.fetchrow("Select * from users where email = $1 and password_hash = $2",
-                               email, password)
+        password_hash: str
+        ) -> asyncpg.Connection.fetchrow:
+    """Create a new user with unique email as a constrain."""
+    return await conn.fetchrow("INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email",
+                    email, password_hash)
 
-async def get_connection(request: Request):
+
+async def get_user_by_email(
+        conn: asyncpg.Connection,
+        email: str
+        ) -> asyncpg.Connection.fetchrow:
+    """Get a user based on their email if exists."""
+    return await conn.fetchrow("Select * from users where email = $1",
+                               email)
+
+async def get_connection(
+        request: Request
+        ) -> asyncpg.Connection:
+    """Get a connection from the database pool to make request."""
     async with request.app.state.db_pool.acquire() as conn:
         yield conn
 
